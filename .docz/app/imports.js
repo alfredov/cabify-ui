c8730334183bad24002630153b751ef882c0b808
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-04-19 14:17:03 -0400

    Remove .docz directory

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'components/Button/Button.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "components-button-button" */ 'components/Button/Button.mdx'
-    ),
-}
